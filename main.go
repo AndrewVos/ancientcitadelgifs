3350470024ea761e34d44be5ead290932832a356
Andrew Vos <andrew.vos@gmail.com>
2015-06-13 13:50:49 +0100

    Upload files directly to S3

--- main.go
+++ main.go
@@ -10,12 +10,16 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/rlmcpherson/s3gof3r"
 
 	"github.com/gorilla/mux"
 )
+
+var bucketName = os.Getenv("S3_BUCKET_NAME")
 
 type JSONError struct {
 	Error string `json:"error"`
@@ -37,7 +41,7 @@ func main() {
 
 	r.Handle("/", http.HandlerFunc(rootHandler))
 	r.Handle("/upload", http.HandlerFunc(uploadHandler))
-	r.Handle("/fetch", http.HandlerFunc(fetchHandler))
+	r.Handle("/{asset}", http.HandlerFunc(assetHandler))
 	http.Handle("/", r)
 	fmt.Printf("Starting on port %v...\n", *port)
 
@@ -126,31 +130,47 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("gifs?"))
 }
 
-func fetchHandler(w http.ResponseWriter, r *http.Request) {
-	gifURL := r.URL.Query().Get("u")
-	if gifURL == "" {
-		serveError(w, "please specify a file to download")
-		return
+func assetHandler(w http.ResponseWriter, r *http.Request) {
+	asset := mux.Vars(r)["asset"]
+
+	contentTypes := map[string]string{
+		".webm": "video/webm",
+		".mp4":  "video/mp4",
+		".png":  "image/png",
+	}
+	for e, t := range contentTypes {
+		if strings.HasSuffix(asset, e) {
+			w.Header().Set("Content-Type", t)
+			break
+		}
 	}
 
-	videoExtension := r.URL.Query().Get("t")
-	if videoExtension != "webm" && videoExtension != "mp4" && videoExtension != "png" {
-		serveError(w, "please choose either mp4, webm, or png extension")
-		return
-	}
-
-	gifPath, err := downloadFile(gifURL)
+	keys, err := s3gof3r.EnvKeys()
 	if err != nil {
 		serveError(w, err.Error())
 		return
 	}
 
-	videoPath, err := convertFile(gifURL, gifPath, videoExtension)
+	s3 := s3gof3r.New("", keys)
+	bucket := s3.Bucket(bucketName)
+
+	reader, _, err := bucket.GetReader(asset, nil)
 	if err != nil {
 		serveError(w, err.Error())
 		return
 	}
-	http.ServeFile(w, r, videoPath)
+
+	_, err = io.Copy(w, reader)
+	if err != nil {
+		serveError(w, err.Error())
+		return
+	}
+
+	err = reader.Close()
+	if err != nil {
+		serveError(w, err.Error())
+		return
+	}
 }
 
 func serveError(w http.ResponseWriter, e string) {
@@ -158,6 +178,32 @@ func serveError(w http.ResponseWriter, e string) {
 	w.Header().Set("Content-Type", "application/json")
 	log.Println("error: " + e)
 	http.Error(w, string(b), http.StatusInternalServerError)
+}
+
+func putToS3(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	keys, err := s3gof3r.EnvKeys()
+	if err != nil {
+		return err
+	}
+
+	s3 := s3gof3r.New("", keys)
+	bucket := s3.Bucket(bucketName)
+	writer, err := bucket.PutWriter(path, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, file)
+	if err != nil {
+		return err
+	}
+	return writer.Close()
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -175,30 +221,36 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("uploading %q to %q\n", gifURL, gifPath)
-
 	width, height, err := getImageDimensions(gifPath)
 	if err != nil {
 		serveError(w, "error getting dimensions "+err.Error())
 		return
 	}
 
-	mp4Query := url.Values{}
-	mp4Query.Set("u", gifURL)
-	mp4Query.Set("t", "mp4")
+	for _, extension := range []string{"webm", "mp4", "png"} {
+		videoPath, err := convertFile(gifURL, gifPath, extension)
+		if err != nil {
+			serveError(w, err.Error())
+			return
+		}
 
-	webmQuery := url.Values{}
-	webmQuery.Set("u", gifURL)
-	webmQuery.Set("t", "webm")
+		fmt.Printf("uploading %q\n", videoPath)
 
-	pngQuery := url.Values{}
-	pngQuery.Set("u", gifURL)
-	pngQuery.Set("t", "png")
+		err = putToS3(videoPath)
+		if err != nil {
+			serveError(w, err.Error())
+			return
+		}
+	}
+
+	mp4Path := strings.TrimSuffix(gifPath, ".gif") + ".mp4"
+	webmPath := strings.TrimSuffix(gifPath, ".gif") + ".webm"
+	pngPath := strings.TrimSuffix(gifPath, ".gif") + ".png"
 
 	uploadResult := UploadResult{
-		MP4URL:  "http://gifs.ancientcitadel.com/fetch?" + mp4Query.Encode(),
-		WEBMURL: "http://gifs.ancientcitadel.com/fetch?" + webmQuery.Encode(),
-		PNGURL:  "http://gifs.ancientcitadel.com/fetch?" + pngQuery.Encode(),
+		MP4URL:  "http://gifs.ancientcitadel.com/" + mp4Path,
+		WEBMURL: "http://gifs.ancientcitadel.com/" + webmPath,
+		PNGURL:  "http://gifs.ancientcitadel.com/" + pngPath,
 		Width:   width,
 		Height:  height,
 	}
