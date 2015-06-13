6d512106689bddbfd252eca2a0f133c24635412e
Andrew Vos <andrew.vos@gmail.com>
2015-06-13 23:53:17 +0100

    Add new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-multitail \
-    -z \
-    -cT ansi -l 'git push ancientcitadelgifs1 master' \
-    -cT ansi -l 'git push ancientcitadelgifs2 master' \
-    -cT ansi -l 'git push ancientcitadelgifs3 master' \
-    -cT ansi -l 'git push ancientcitadelgifs4 master' \
-    -cT ansi -l 'git push ancientcitadelgifs5 master'
+COMMANDS='
+git push heroku master
+git push ancientcitadelgifs1 master
+git push ancientcitadelgifs2 master
+git push ancientcitadelgifs3 master
+git push ancientcitadelgifs4 master
+git push ancientcitadelgifs5 master
+'
+echo "$COMMANDS" | tr '\n' '\0' | xargs --null -n 1 -P 6 bash -c
